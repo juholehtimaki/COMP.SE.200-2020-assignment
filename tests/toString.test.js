e301a132dc4713996d9640d1f5bb7d45b411c73c
Juho Lehtimaki <juho.a.lehtimaki@gmail.com>
2020-11-04 18:54:05 +0200

    trigger

--- tests/toString.test.js
+++ tests/toString.test.js
@@ -1,7 +1,5 @@
 import toString from "../src/toString.js";
 
-//Random comment to trigger the build
-
 describe("toString.js tests", () => {
   test("should stringify a number", () => {
     expect(toString(5)).toBe("5");
